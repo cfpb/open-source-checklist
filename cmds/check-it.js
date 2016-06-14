cce7948b36488c64a03cd3a311451f0dbd06bad4
Catherine Farman <Catherine.Farman@cfpb.gov>
2016-06-14 17:25:12 -0400

    TERMS, CONTRIBUTING, CHANGELOG check functions

--- cmds/check-it.js
+++ cmds/check-it.js
@@ -3,7 +3,6 @@
 var fs = require('fs');
 var path = require('path');
 var request = require('request');
-var semver = require('semver');
 var logger = require('winston-color');
 var npm = require('npm');
 
@@ -14,11 +13,11 @@ module.exports = function(program) {
 
   program
     .command('*')
-    .description('Check your node module for security issues.')
+    .description('Check that your project repo meets CFPB\'s open source checklist requirements.')
     .action(function(loc) {
       location = loc || '.';
       fix = program.fix;
-      [checkTERMS, checkCONTRIBUTING].forEach(function(func) {
+      [checkPII, checkTERMS, checkCONTRIBUTING, checkCHANGELOG].forEach(function(func) {
         func(function(err, msg) {
           if (err) {
             return logger.error(err);
@@ -31,10 +30,13 @@ module.exports = function(program) {
 
 };
 
+function checkPII(cb) {
+  // Has PII been removed?
+}
+
 function checkTERMS(cb) {
   fs.readFile(path.join(location, './TERMS.md'), 'utf8', function (err, data) {
     if (err) return handleIssue(1, cb);
-    if (data.indexOf('save-exact') < 0) return handleIssue(2, cb);
     cb(null, 'Your `TERMS.md` file looks good.');
   });
 }
@@ -45,46 +47,50 @@ function checkCONTRIBUTING(cb) {
     cb(null, 'Your CONTRIBUTING.md file seems to be in order.');
   });
 }
-// @todo: update handleIssue to clone TERMS + CONTRIBUTING from https://github.com/cfpb/open-source-project-template
+
+function checkCHANGELOG(cb) {
+  fs.readFile(path.join(location, './CHANGELOG.md'), 'utf8', function (err, data) {
+    if (err) return handleIssue(3, cb);
+    cb(null, 'Your CHANGELOG.md file is good to go.');
+  });
+}
 
 function handleIssue(type, cb, data) {
   switch (type) {
     case 1:
       if (fix) {
-        request('https://raw.githubusercontent.com/cfpb/generator-cf/master/app/templates/_npmrc')
+        request('https://raw.githubusercontent.com/cfpb/open-source-project-template/master/TERMS.md')
           .on('error', function(err) {
             cb(err, null)
           })
-          .pipe(fs.createWriteStream(path.join(location, './.npmrc')))
-        return cb(null, 'No .npmrc file found. I created one for you.');
+          .pipe(fs.createWriteStream(path.join(location, './TERMS.md')))
+        return cb(null, 'No TERMS.md file found. I created one for you.');
       }
-      cb('No .npmrc file found. Please create one and add `save-exact=true` to it.', null);
+      cb('No TERMS.md file found. Please create one based on the Open Source Project Template: https://github.com/cfpb/open-source-project-template/blob/master/TERMS.md', null);
       break;
     case 2:
       if (fix) {
-        fs.appendFile(path.join(location, './.npmrc'), '\nsave-exact=true', function(err) {
-          if (err) cb(err, null);
-        });
-        return cb(null, 'I added `save-exact=true` to your .npmrc file.');
+        request('https://raw.githubusercontent.com/cfpb/open-source-project-template/master/CONTRIBUTING.md')
+          .on('error', function(err) {
+            cb(err, null)
+          })
+          .pipe(fs.createWriteStream(path.join(location, './CONTRIBUTING.md')))
+        return cb(null, 'No CONTRIBUTING.md file found. I created one for you.');
       }
-      cb('Please add `save-exact=true` to your .npmrc file.', null);
+      cb('No CONTRIBUTING.md file found. Please create one based on the Open Source Project Template: https://github.com/cfpb/open-source-project-template/blob/master/CONTRIBUTING.md', null);
       break;
     case 3:
-      cb('No `package.json` file found. Are you sure this is a node module?', null);
+      if (fix) {
+        request('https://raw.githubusercontent.com/cfpb/open-source-project-template/master/CHANGELOG.md')
+          .on('error', function(err) {
+            cb(err, null)
+          })
+          .pipe(fs.createWriteStream(path.join(location, './CHANGELOG.md')))
+        return cb(null, 'No CHANGELOG.md file found. I created one for you.');
+      }
+      cb('No CHANGELOG.md file found. Please create one based on the Open Source Project Template: https://github.com/cfpb/open-source-project-template/blob/master/CHANGELOG.md', null);
       break;
     case 4:
-      if (fix) {
-        var newVersion = manifest.dependencies[data].replace(/^(>|~|\^|\*)|(.*(<|>|=))/, '');
-        if (semver.valid(newVersion)) {
-          manifest.dependencies[data] = newVersion;
-          return cb(null, 'I pinned ' + data + ' to ' + newVersion + '.');
-        } else {
-          return cb('I tried to pin ' + data + ' but I failed. Please pin this dependency.', null);
-        }
-      }
-      cb(data + '\'s version (' + manifest.dependencies[data] + ') has a loose range specifier in package.json. Please pin it.', null);
-      break;
-    case 5:
       if (fix) {
         return npm.load({loglevel: 'error'}, function(err, npm) {
           if (err) return logger.error(err);
